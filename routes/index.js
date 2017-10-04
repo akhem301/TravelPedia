0894cd37678e76cefaea69ea4f4395518d016707
akhem301 <akhem301@gmail.com>
2017-10-04 08:04:09 +0530

    Backend synchronization: login signup

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+// Get Homepage
+router.get('/', function(req, res){
+	res.render('index');
+});
+
+
+// router.get('/users', function(req, res){
+// 	//console.log('Reached tours');
+// 	res.render('users');
+// });
+
+// function ensureAuthenticated(req, res, next){
+// 	if(req.isAuthenticated()){
+// 		return next();
+// 	} else {
+// 		//req.flash('error_msg','You are not logged in');
+// 		res.redirect('/users/login');
+// 	}
+// }
+
+module.exports = router;
---
+++ routes/tours.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res){
+	//console.log('Reached tours');
+	res.render('tours');
+});
+
+// router.get('/users', function(req, res){
+// 	//console.log('Reached tours');
+// 	res.render('users');
+// });
+
+// function ensureAuthenticated(req, res, next){
+// 	if(req.isAuthenticated()){
+// 		return next();
+// 	} else {
+// 		//req.flash('error_msg','You are not logged in');
+// 		res.redirect('/users/login');
+// 	}
+// }
+
+module.exports = router;
